b05cae4bb6625d3efac474f0091b791116f58c39
Michael Loukeris <loukerismichalis@gmail.com>
2019-11-08 14:09:30 +0200

    Make Sub-Heading inline with existing solemn declaration

--- generate_declaration.py
+++ generate_declaration.py
@@ -16,7 +16,7 @@ from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
-# from endesive import pdf as endesivepdf
+from endesive import pdf as endesivepdf
 
 import datetime
 import argparse
@@ -99,6 +99,12 @@ STYLES.add(ParagraphStyle(name='DeclBold',
 STYLES.add(ParagraphStyle(name='DeclHeading',
                           fontName='Roboto-Bold',
                           fontSize=16,
+                          alignment=TA_CENTER,
+                          spaceAfter=5))
+
+STYLES.add(ParagraphStyle(name='DeclSubHeading',
+                          fontName='Roboto-Bold',
+                          fontSize=9,
                           alignment=TA_CENTER,
                           spaceAfter=5))
 
@@ -490,6 +496,11 @@ def make_heading(element, contents):
         elements.append(Paragraph(pcontent, STYLES["DeclHeading"]))
 
 
+def make_subheading(element, contents):
+    for pcontent in contents:
+        elements.append(Paragraph(pcontent, STYLES["DeclSubHeading"]))
+
+
 def make_intro(elements, contents):
     elements.append(Spacer(1, 12))
     elements.append(Paragraph(contents, STYLES["Warning"]))
@@ -578,7 +589,7 @@ if __name__ == "__main__":
     payload = load_payload('data.json')
 
     make_heading(elements, [TITLE])
-    make_heading(elements, [LAW])
+    make_subheading(elements, [LAW])
     elements.append(Spacer(1, 12))
     make_intro(elements, WARNING)
     make_human_signature(elements, payload)
